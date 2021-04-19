0f90c93b81db406c43a4fba1d8550a986189417c
Administrator <Administrator@samadal>
2021-04-19 11:36:04 +0900

    문제1풀고 저장

---
+++ GitStudy/src/exam/Question1.java
@@ -0,0 +1,12 @@
+package exam;
+
+public class Question1 {
+	public static void main(String[] args) {
+		System.out.println("====================");
+		System.out.println("        /)/)");
+		System.out.println("       (  ..)");
+		System.out.println("       (  >¢½");
+		System.out.println(" Have a Good Time.");
+		System.out.println("====================\n");
+	}
+}
